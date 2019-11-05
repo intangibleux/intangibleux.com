f2b96c6ff49a1d21e337f54c112108e7e05a82c8
Melisa Hildt <melhildt@gmail.com>
2019-11-05 17:49:14 -0300

    Actualización de URL

--- js/ultimo.js
+++ js/ultimo.js
@@ -1,5 +1,5 @@
 $(function() {
-    $.getJSON('https://freelance.axelvf.com.ar/observando/mailchimp.php')
+    $.getJSON('https://freelance.axelvf.com.ar/intangible/mailchimp.php')
         .success(function(data) {
             window.location.href = data[0].link;
         })
