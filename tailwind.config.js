e95b763c0c97cc1cf3fff85e67b8aefb59649071
Melisa Hildt <melisahildt@me.com>
2019-12-04 11:11:38 -0300

    Primera versión draft

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  theme: {
+    colors: {
+      blue: {
+        dark: '#001F3F',
+        light: '#8291A1',
+        lightlight: '#E5E9EC',
+      },
+      aqua: {
+        dark: '#8FD6CD',
+        lightlight: '#F7FCFC',
+        light: '#D3F1EE',
+      },
+      yellow: {
+        dark: '#EEBB55',
+        light: '#FBD68C',
+      },
+      white: '#FFF',
+    },
+    fontFamily: {
+      display: ['Livvic', 'sans-serif'],
+      body: ['DM Sans', 'sans-serif'],
+    },
+  },
+  variants: {},
+  plugins: []
+}
